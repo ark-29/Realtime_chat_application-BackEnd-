ffd9505ccf93cd78dc5503b6e3d5d6074e01eefa
A ABHIRAM KRISHNA <abhiramavanapu29@gmail.com>
2025-10-26 16:15:58 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel, create_engine, Session
 
-database_url = "mysql+pymysql://root:ark568@localhost/chat_app"
+database_url = "mysql+pymysql://root:<password>@<hostname>/<dbname>"
 
 Engine = create_engine(database_url, echo=True)
 
@@ -10,3 +10,4 @@ async def get_session():
 
 async def create_db_tables():
     SQLModel.metadata.create_all(Engine)
+
